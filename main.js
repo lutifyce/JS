2c5383cdad23a456c4f9f28627fa8c3b79ac0358
lutifyce <lutifyce@mail.ru>
2023-02-21 21:02:56 +0300

    25

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-let num1 = 10;
-let num2 = 20;
-alert(String(num1) + String(num2));
-//
-let a=1232133;
-alert(String(a).length);
-//
-let d=1232133;
-let c=1232133;
-alert(String(c).length+String(d).length)
+//4
+//2
+//0
+//1
+//1true
+//true1
+//true1
