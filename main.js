1ef6e65e760b5a879e2a9ad6e7efb0062150e3c9
lutifyce <lutifyce@mail.ru>
2023-02-21 16:39:33 +0300

    6

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-"use strict";// strict use
-/*
-alert
-*/
-alert('Привет, мир!');
+let num;
+num= 123;
+alert(123);
+//
+let abc = 1;
+let bbc = 1;
+let bbb = 1;
+//
+let cbc=1, ccc=1, cac=1;
+//
+let cc, aa, bb;
+cc=1;
+bb=1;
+aa=1;
+//
+
+let a;
+a=10;
+alert(a);
+a=20;
+alert(a);
+
