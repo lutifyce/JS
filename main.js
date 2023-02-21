a1510a8cf8bbfc24b389d8a070f5d2dc067dc254
lutifyce <lutifyce@mail.ru>
2023-02-21 16:41:16 +0300

    7

--- main.js
+++ main.js
@@ -1,22 +1,2 @@
-let num;
-num= 123;
-alert(123);
-//
-let abc = 1;
-let bbc = 1;
-let bbb = 1;
-//
-let cbc=1, ccc=1, cac=1;
-//
-let cc, aa, bb;
-cc=1;
-bb=1;
-aa=1;
-//
-
-let a;
-a=10;
-alert(a);
-a=20;
-alert(a);
-
+let a=1+2+3;
+alert(a)
