95a89d4af6fe88c35fe5f618a17d3479aeb7fccf
lutifyce <lutifyce@mail.ru>
2023-02-21 17:02:42 +0300

    Js_n10

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-let a=10,b=2;
-alert('summ=' + a + b)
-alert('diff=' + a - b)
-alert('div=' + a / b)
-alert('multipl=' + a * b)
-//
-let c=10,d=5;
-let result=c+d;
-alert(result)
-//
-let aa=1, bb=2, cc=3;
-alert(aa+bb+cc);
-//
-let aaa=10, bbb=5, ccc=aaa=bbb, dd=7;
-let result11=dd+ccc;
-alert(result1)
+//20
+//23
+//6
+//9
+//8
+//8
+//25
+//15
+//36
+//4
+//4
+//4
