e18635a15ebb8fa7e552f4dff7261218930cc692
lutifyce <lutifyce@mail.ru>
2023-02-22 09:55:23 +0300

    26

--- main.js
+++ main.js
@@ -1,7 +1,64 @@
-//4
-//2
-//0
-//1
-//1true
-//true1
-//true1
+/*
+let test = Boolean(3);
+alert(test);
+
+let test = Boolean(0);
+alert(test);
+
+let test = Boolean(-1);
+alert(test);
+
+let test = Boolean(-0);
+alert(test);
+
+let test = Boolean(+0);
+alert(test);
+
+let test = Boolean('abc');
+alert(test);
+
+let test = Boolean('');
+alert(test);
+
+let test = Boolean('0');
+alert(test);
+
+let test = Boolean(true);
+alert(test);    
+
+let test = Boolean(false);
+alert(test);
+
+let test = Boolean('true');
+alert(test);
+
+let test = Boolean('false');
+alert(test);
+
+let test = Boolean(null);
+alert(test);
+
+let test = Boolean('null');
+alert(test);
+
+let test = Boolean(undefined);
+alert(test);
+
+let test = Boolean('undefined');
+alert(test);
+
+let test = Boolean(NaN);
+alert(test);
+
+let test = Boolean('NaN');
+alert(test);
+
+let test = Boolean(3 * 'abc');
+alert(test);
+
+let test = Boolean(Infinity);
+alert(test);
+
+let test = Boolean(1 / 0);
+alert(test);
+*/
