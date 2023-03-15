b60686cb570c39c3bf6dd1ed0f576167cd5120fd
lutifyce <lutifyce@mail.ru>
2023-03-15 21:13:25 +0300

    28.1

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-"use strict";
-let a, b, c, d, result, str; // объявляем все 3 переменные методом в одну строку
-
-// Присваиваем переменным значения:
-a = 'abcde';
-alert(a[0]+ ' ' + a[2]+ ' ' +a[4]);
-b = a[4] + a[3] + a[2] + a[1] + a[0];
-alert(b);
-str = 'abcde';
-let num = 3;
-alert(str[num]);
+let str = '12345avc';
+alert(str[str.length - 1]);
+alert(str1[str1.length - 2]);
+alert(str2[str2.length - 3]);
