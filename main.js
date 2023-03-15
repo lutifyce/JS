d375a57aab3c7f673b97e035ff4f98caf2b1e380
lutifyce <lutifyce@mail.ru>
2023-03-15 21:22:35 +0300

    33

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-alert(0.1 * 0.2);
-alert(0.3 * 0.1);
+alert('возраст:' + prompt('возраст?')); 
+
+let num1 = prompt('1');
+let num2 = prompt('2');
+alert(Number(num1) + Number(num2)); 
+
+let storona = prompt('сторона квадрата: = ');
+alert(storona*storona);
+
+let num3 = prompt('Введите первое число');
+let num4 = prompt('Введите второе число');
+alert(Number(num3) + Number(num4)); 
