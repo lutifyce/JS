968e2f4bbc5187b11e0751bdd38387aaf0136342
lutifyce <lutifyce@mail.ru>
2023-03-15 21:19:31 +0300

    30

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-let a = '12345'; 
-alert(Number(a[0]) + Number(a[1])+Number(a[2]) + Number(a[3])+Number(a[4]))
-let b =12345
-for (let index = 0; index < b.length; index++) {
-  bbb += Number(b[index]);
-};
-for (let index = 0; index < b.length; index++) {
-  bcb *= Number(b[index]);
-str=String(b);
-alert(bbb);
-alert(bcb);
-alert(reverseString(str));
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
