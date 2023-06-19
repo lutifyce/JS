f0b4c90ee82b3302af63cb14a5d71d4352b9748d
lutifyce <lutifyce@mail.ru>
2023-06-19 09:56:16 +0300

    36

--- main.js
+++ main.js
@@ -1,53 +1,12 @@
 "use strict";
 
-let num1 = 1;
-let num2 = 2;
-console.log('сумма: ' + (num1 + num2));
-
-let a1 = 1;
-let b = 2;
-console.log(a1 + b);
-
-let num = '123';
-let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
-console.log(sum);
-
-let num3 = 123;
-num3 = String(num3);
-console.log(num3[0]);
-
-let a = 0;
-console.log(++a);
-
-let num4 = 123;
-num4 =  String(num4);
-console.log(num4.length);
-
-let a3 = 24 * 60 * 60;
-console.log(a3);
-
-let num5 = 123;
-let str = String(num5);
-console.log(str.length);
-
-let num6 = 123;
-let str2 = String(num6);
-console.log(Number(str2[str2.length - 1]));
-
-let num7 = 123;
-let str3 = String(num7);
-console.log(str3.length);
-
-let num8 = 123;
-let str4 = String(num8);
-console.log(str4[str4.length - 1]);
-
-let a4 = '123';
-let b2 = '456';
-let s = Number(a4) + Number(b2);
-console.log(s);
-
-let aaa = 1;
-let bbb = 2;
-let ccc = 3;
-console.log(aaa + bbb + ccc);
+alert(60 * 60 * 24);
+alert(60 * 60 * 24 * 30);
+alert(60 * 60 * 24 * 365);
+alert(60 * 24);
+alert(60 * 365 * 24);
+alert(1024 * 1024);
+alert(1024 * 1024 * 1024);
+alert(1024 * 1024 * 1024 * 10);
+alert(1024 * 1024 * 1000);
+alert(1024 * 1024 * 1024 * 1000);
