6099e599eab9756ba2d1435d35f53006002be4bb
lutifyce <lutifyce@mail.ru>
2023-06-19 10:03:34 +0300

    49

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 "use strict";
 
-let obj = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'};
-document.write(obj[1], '<br>')
-document.write(obj[2], '<br>')
-document.write(obj[3], '<br>')
-document.write(obj[4], '<br>')
-document.write(obj[5], '<br>')
-document.write(obj[6], '<br>')
-document.write(obj[7])
+let obj = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'Jule', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'};
+console.log(obj);
