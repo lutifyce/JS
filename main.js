9242058ce7db80469906e87d000eb69146e744c2
lutifyce <lutifyce@mail.ru>
2023-06-19 10:02:43 +0300

    47

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
-delete arr[1];
-delete arr[4];
-document.write(arr.length);
+let arr = [1, 2, 3, 4, 5];
+console.log(arr[arr.length - 1]);
+console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]); 
+console.log(arr.length);
+console.log(arr.length);
