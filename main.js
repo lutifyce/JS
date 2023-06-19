cbcf39d2b24d1dfbefbf05daba5c062a28dbe614
lutifyce <lutifyce@mail.ru>
2023-06-19 09:58:47 +0300

    41

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 "use strict";
 
-let arr = ['a', 'b', 'c',];
-document.write(arr.length, '<br>')
+let arr = ['a', 'b', 'c'];
+
+arr[0] = '1';
+arr[1] = '-2';
+arr[2] = '-3';
+document.write(arr, '<br>');
+
+let arr1 = [1, 2, 3];
+arr1[0] += 1;
+arr1[1] += 1;
+arr1[2] += 1;
+document.write(arr1, '<br>');
 
 let arr2 = [1, 2, 3];
-document.write(arr2[arr2.length - 1])
+arr2[0]++;
+arr2[1]++;
+arr2[2]++;
+document.write(arr2, '<br>');
