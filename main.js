8788da0a14585143e15b8104a9d85fd39fa7dfa5
lutifyce <lutifyce@mail.ru>
2023-06-19 10:00:49 +0300

    43

--- main.js
+++ main.js
@@ -2,13 +2,6 @@
 
 let arr = [];
 
-arr[0] = '1';
-arr[1] = '2';
-arr[2] = '3';
-document.write(arr, '<br>');
-
-let arr1 = [1, 2, 3];
-
-arr1[3] = '4';
-arr1[4] = '5';
-document.write(arr1);
+arr[3] = 'a';
+arr[8] = 'b';
+alert(arr.length);
