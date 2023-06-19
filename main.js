5c3b6e4e231b1384d913195b37e17b1ed254d4ba
lutifyce <lutifyce@mail.ru>
2023-06-19 10:03:15 +0300

    48

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 "use strict";
 
-let arr = [1, 2, 3, 4, 5];
-console.log(arr[arr.length - 1]);
-console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4]); 
-console.log(arr.length);
-console.log(arr.length);
+let obj = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7: 'Sunday'};
+document.write(obj[1], '<br>')
+document.write(obj[2], '<br>')
+document.write(obj[3], '<br>')
+document.write(obj[4], '<br>')
+document.write(obj[5], '<br>')
+document.write(obj[6], '<br>')
+document.write(obj[7])
