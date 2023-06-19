29a9ae04e1b6741689a2be71ddb0bdec6eb8d267
lutifyce <lutifyce@mail.ru>
2023-06-19 09:55:47 +0300

    35

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
 "use strict";
 
-document.write('lutifyce<br>');
-document.write('<i>lutifyce</i>');
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ' + (num1 + num2));
 
-let str = 'luti2';
-document.write('<i><br>' + str + '</i></br>');
+let a1 = 1;
+let b = 2;
+console.log(a1 + b);
 
-document.write('1<br>');
-document.write('2<br>');
-document.write('3<br>');
-document.write('4<br>');
-document.write('5<br>');
+let num = '123';
+let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+console.log(sum);
+
+let num3 = 123;
+num3 = String(num3);
+console.log(num3[0]);
+
+let a = 0;
+console.log(++a);
+
+let num4 = 123;
+num4 =  String(num4);
+console.log(num4.length);
+
+let a3 = 24 * 60 * 60;
+console.log(a3);
+
+let num5 = 123;
+let str = String(num5);
+console.log(str.length);
+
+let num6 = 123;
+let str2 = String(num6);
+console.log(Number(str2[str2.length - 1]));
+
+let num7 = 123;
+let str3 = String(num7);
+console.log(str3.length);
+
+let num8 = 123;
+let str4 = String(num8);
+console.log(str4[str4.length - 1]);
+
+let a4 = '123';
+let b2 = '456';
+let s = Number(a4) + Number(b2);
+console.log(s);
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+console.log(aaa + bbb + ccc);
